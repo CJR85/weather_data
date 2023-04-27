ae11dbb60caf7ab79f5abba7fcaa8f0e7e062262
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-27 23:31:07 +0100

    Squirrel Census data

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
+# import pandas
+
+# data = pandas.read_csv("weather_data.csv")
+# # print(type(data))
+# # print(type(data["temp"]))
+
+# # data_dict = data.to_dict()
+# # print(data_dict)
+
+# # temp_list = data["temp"].to_list()
+# # print(len(temp_list))
+
+# # # average = sum(temp_list) / len(temp_list)
+# # # print(average)
+
+# # print(data["temp"].mean())
+# # print(data["temp"].max())
+
+# # # Get data in Columns
+# # print(data["condition"])
+# # print(data.condition)
+
+# # Get data in Row
+# # print(data[data.day == "Monday"])
+# # print(data[data.temp == data.temp.max()])
+
+# # monday = data[data.day == "Monday"]
+# # monday_temp = int(monday.temp)
+# # monday_temp_F = monday_temp * 9/5 + 32
+
+# # Create a dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 67]
+# }
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-# print(type(data))
-# print(type(data["temp"]))
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
 
-# data_dict = data.to_dict()
-# print(data_dict)
-
-# temp_list = data["temp"].to_list()
-# print(len(temp_list))
-
-# # average = sum(temp_list) / len(temp_list)
-# # print(average)
-
-# print(data["temp"].mean())
-# print(data["temp"].max())
-
-# # Get data in Columns
-# print(data["condition"])
-# print(data.condition)
-
-# Get data in Row
-# print(data[data.day == "Monday"])
-# print(data[data.temp == data.temp.max()])
-
-# monday = data[data.day == "Monday"]
-# monday_temp = int(monday.temp)
-# monday_temp_F = monday_temp * 9/5 + 32
-
-# Create a dataframe from scratch
 data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 67]
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
 }
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrrel_count.csv")
